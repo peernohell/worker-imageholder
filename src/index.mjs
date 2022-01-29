d8141a8e36b728c6c9fe3efc088fb273625b4190
Francois Lecroart <peernohell@gmail.com>
2022-01-29 11:53:30 +0100

    clean up code

--- src/index.mjs
+++ src/index.mjs
@@ -29,161 +29,20 @@ export default {
   },
 }
 
-// Create the bézier paths for each of the glyphs.
-// Note that the .notdef glyph is required.
-const notdefGlyph = new opentype.Glyph({
-  name: '.notdef',
-  unicode: 0,
-  advanceWidth: 650,
-  path: new opentype.Path()
-});
-
-const aPath = new opentype.Path();
-aPath.moveTo(100, 0);
-aPath.lineTo(100, 700);
-// more drawing instructions...
-const aGlyph = new opentype.Glyph({
-  name: 'A',
-  unicode: 65,
-  advanceWidth: 650,
-  path: aPath
-});
-
-const bPath = new opentype.Path();
-// bPath.moveTo(100, 0);
-bPath.moveTo(10, 0);
-bPath.lineTo(10, 10);
-bPath.lineTo(20, 10);
-bPath.lineTo(20, 50);
-bPath.lineTo(0, 10);
-// more drawing instructions...
-const bGlyph = new opentype.Glyph({
-  name: 'B',
-  unicode: 66,
-  advanceWidth: 650,
-  path: bPath
-});
-
-const glyphs = [notdefGlyph, aGlyph, bGlyph];
-// const font = new opentype.Font({
-//   familyName: 'Roboto',
-//   styleName: 'Medium',
-//   unitsPerEm: 1000,
-//   ascender: 800,
-//   descender: -200,
-//   glyphs: glyphs,
-// });
-
-const fn1 = () => {
+const generateImage = () => {
   var img = PImage.make(500,50);
-  console.log('img ok');
+  console.log('generateImage: start');
   var ctx = img.getContext('2d');
-  console.log('ctx ok');
+  console.log('generateImage: ctx created');
+
   ctx.fillStyle = '#AAAAAA';
   ctx.fillRect(0,0,500,50);
   ctx.fillStyle = '#FF3333';
   ctx.font = "18px 'Roboto'";
-  console.log('ctx.font ok');
-  ctx.fillText('hello world  abcdefg', 40, 40);
-
-  return img;
-}
-
-const fn2 = () => {
-  var img = PImage.make(100,100);
-  var ctx = img.getContext('2d');
-  ctx.fillStyle = '#AAAAAA';
-  ctx.fillRect(0,0,100,100);
-
-  ctx.fillStyle = '#00FF00';
-  ctx.beginPath();
-  ctx.arc(50,50,40,0,Math.PI*2,true); // Outer circle
-  ctx.closePath();
-  ctx.fill();
-
-  return img;
-}
-
-const fn22 = () => {
-  var img = PImage.make(100,100);
-  var ctx = img.getContext('2d');
-  ctx.fillStyle = '#AAAAAA';
-  ctx.fillRect(0,0,100,100);
-
-  ctx.fillStyle = '#00FF00';
-  ctx.beginPath();
-
-  // ctx.arc(50,50,40,0,Math.PI*2,true)
-  const x = 50;
-  const y = 50;
-  const rad = 40;
-  let start = 0;
-  let end = Math.PI * 2;
-  const anticlockwise = false;
-
-  function calcPoint(angle) {
-      let px = x + Math.cos(angle)*rad;
-      let py = y + Math.sin(angle)*rad;
-      return [px, py];
-  }
-
-  if(start > end) end += Math.PI*2;
-
-  let step = Math.PI / 16
-  if (anticlockwise) {
-      let temp = end;
-      end = start + Math.PI * 2;
-      start = temp;
-  }
-  let [p1, p2] = calcPoint(start);
-  console.log('start', { p1, p2 })
-  ctx.moveTo(p1, p2);
-  for (let a = start; a <= end; a += step) {
-    ([p1, p2] = calcPoint(a));
-    console.log('step', { p1, p2 })
-    ctx.lineTo(p1, p2);
-  }
-  ([p1, p2] = calcPoint(end));
-  console.log('end', { p1, p2 })
-  ctx.lineTo(p1, p2);
+  console.log('generateImage: ctx.font set');
   
-  ctx.closePath();
-  ctx.fill();
-
-  return img;
-}
-
-const fn3 = () => {
-  var img = PImage.make(100,100);
-  var ctx = img.getContext('2d');
-  ctx.fillStyle = '#FFFFFF';
-  ctx.fillRect(0,0,10,10);
-
-  ctx.font = "12px 'Roboto'";
-  console.log('ctx.font ok');
-  ctx.fillText("A", 8, 8);
-
-  return img;
-}
-
-const fn4 = () => {
-  var img = PImage.make(150,150);
-  var ctx = img.getContext('2d');
-  ctx.fillStyle = '#AAAAAA';
-  ctx.fillRect(0, 0, 150, 150);
-
-  ctx.fillStyle = '#FF0000';
-  ctx.fillRect(25, 25, 100, 100);
-  ctx.clearRect(45, 45, 60, 60);
-  ctx.fillStyle = '#0000FF';
-  ctx.strokeRect(50, 50, 50, 50);
-
-
-  ctx.beginPath();
-  ctx.moveTo(75, 50);
-  ctx.lineTo(100, 75);
-  ctx.lineTo(100, 25);
-  ctx.fill();
+  ctx.fillText('hello world', 40, 40);
+  console.log('generateImage: ctx.fillText done');
 
   return img;
 }
@@ -213,8 +72,7 @@ const handleRequest = async (req, env, writable) => {
   // special font loading
   const url = new URL(req.url);
  const accept = req.headers.get('accept');
- if (req.method === 'GET' &&
-     url.pathname.startsWith('/fonts.gstatic.com/')) {
+ if (req.method === 'GET' && url.pathname.startsWith('/fonts.gstatic.com/')) {
  
    // Proxy the font file requests
    return proxyRequest('https:/' + url.pathname, req);
@@ -249,7 +107,7 @@ const handleRequest = async (req, env, writable) => {
       fnt.font = font;
       fnt.loaded = true;
 
-      const img = fn1();
+      const img = generateImage();
       // console.log('starting encodingPNG ...', img.width, img.height, { pixel: img.getPixelRGBA(0,0), isFFFF: img.getPixelRGBA(0,0) === 0xffffffff, data: img.data });
       // await PImage.encodePNGToStream(img, writable);
     // return img.data;
